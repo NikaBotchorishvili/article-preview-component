7369c56f060e60115c27442a585fe7795db4a2bb
Nika Botchorishvili <nikabotchorishvili@gmail.com>
2024-01-21 17:51:23 +0400

    feat: Built out responsive design for all devices and added javascript functionality to give the ability to users to toggle sharebox

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,26 @@
-let shareButton = document.getElementById('shareButton');
-let shareBox = document.getElementById('shareBox');
+let container = document.getElementById('container');
+let shareButtonDesktop = document.getElementById('shareButtonDesktop');
+let shareButtonMobile = document.getElementById('shareButtonMobileOpen');
+let information = document.getElementById('information');
+let shareBoxDesktop = document.getElementById('shareBoxDesktop');
+let shareBoxMobile = document.getElementById('shareBoxMobile');
 
-shareButton.addEventListener("click", toggleShareBox)
-function toggleShareBox() {
-    console.log("ben")
-    shareBox.style.display = (shareBox.style.display === 'none' || shareBox.style.display === '') ? 'flex' : 'none';
+shareButtonDesktop.addEventListener("click", toggleShareBoxDesktop)
+
+shareButtonMobile.addEventListener("click", openShareBoxMobile)
+
+function toggleShareBoxDesktop() {
+    shareBoxDesktop.style.display = (shareBoxDesktop.style.display === 'none' || shareBoxDesktop.style.display === '') ? 'flex' : 'none';
 }
+function openShareBoxMobile() {
+    information.style.display = 'none';
+    container.style.paddingInline = 0;
+    container.style.paddingBottom = 0;
+    shareButtonMobile.style.display = "none";
+    shareBoxMobile.style.display = "flex"
+}
+function closeShareBoxMobile() {
+    
+
+}
+
